ccc0f39dfef0f77797d20d15b74090d6a3754da3
descomkraneoz <descom666999@gmail.com>
2019-09-27 13:03:11 +0200

    2do commit haciendo agenda

--- app/src/main/java/net/iesseveroochoa/manuelmartinez/practica2/MainActivity.java
+++ app/src/main/java/net/iesseveroochoa/manuelmartinez/practica2/MainActivity.java
@@ -2,11 +2,17 @@ package net.iesseveroochoa.manuelmartinez.practica2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvNombre;
+    Button btAgenda;
+    Button btCalculadora;
+    Button btReceta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvNombre=findViewById(R.id.tvNombre);
+        btAgenda=findViewById(R.id.btAgenda);
+        btCalculadora=findViewById(R.id.btCalculadora);
+        btReceta=findViewById(R.id.btReceta);
+
+        btAgenda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,AgendaActivity.class);
+
+                //intent.putExtra(AgendaActivity.EXTRA_NOMBRE, etNombre.getText().toString());
+
+                startActivity(intent);
+            }
+        });
     }
 }
